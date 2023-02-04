82061cae8e1e10b115ea0f071b631a46c1c6b620
GeorgiyKuzora <rafale87@gmail.com>
2023-02-04 17:04:15 +0300

    Remake C# Sem3 task1, to compare integers instead of chars

--- 2_c-sharp/Seminar_3/home_work/task1.cs
+++ 2_c-sharp/Seminar_3/home_work/task1.cs
@@ -1,15 +1,10 @@
 ﻿Console.Clear();
-
-Console.Write("Введите пятизначное число: ");
-string N = Console.ReadLine();
-bool is_pal = true;
-for (int i = 0; i < N.Length/2; i++){
-    if (N[i] != N[N.Length-1-i]){
-        Console.WriteLine("Число не палиндром");
-        is_pal = false;
-        break;
-    }
+bool notPenta = true;
+int N = 0;
+while (notPenta){
+    Console.Write("Введите пятизначное число: ");
+    N = int.Parse(Console.ReadLine());
+    if (N/10000 != 0 && N/100000 == 0) notPenta = false;
 }
-if (is_pal){
-    Console.WriteLine("Число палиндром");
-}
+if (N/10000 == N%10 && (N%10000)/1000 == (N%100)/10) Console.WriteLine("Число палиндром");
+else Console.WriteLine("Число не палиндром");
