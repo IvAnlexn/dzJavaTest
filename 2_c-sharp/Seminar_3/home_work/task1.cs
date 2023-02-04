e9ecac160d053cffe94e67c57b27419a82ee318a
GeorgiyKuzora <rafale87@gmail.com>
2023-02-04 16:28:25 +0300

    Remove unnessesary iteration from C# Sem3 task1

--- 2_c-sharp/Seminar_3/home_work/task1.cs
+++ 2_c-sharp/Seminar_3/home_work/task1.cs
@@ -3,7 +3,7 @@
 Console.Write("Введите пятизначное число: ");
 string N = Console.ReadLine();
 bool is_pal = true;
-for (int i = 0; i <= N.Length/2; i++){
+for (int i = 0; i < N.Length/2; i++){
     if (N[i] != N[N.Length-1-i]){
         Console.WriteLine("Число не палиндром");
         is_pal = false;
