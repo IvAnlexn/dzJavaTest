63809be8f75a72552ba37d0b4a5ba227dd856086
GeorgiyKuzora <rafale87@gmail.com>
2023-02-15 20:29:05 +0300

    Add home work for C-sharp Seminar 6

---
+++ 2_c-sharp/Seminar_6/home_work/task41.cs
@@ -0,0 +1,28 @@
+﻿int GetNumberPositive(int[] arr){
+    // Method gets answer
+    int i = 0;
+    foreach(int n in arr){
+        if (n > 0) i++;
+    }
+    return i;
+}
+
+int[] StringToIntArray(string stringElements)
+{ string[] stringArray = stringElements.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[stringArray.Length];
+    for(int i = 0; i < res.Length; i++)
+    { res[i] = int.Parse(stringArray[i]);
+    } return res; }
+
+string Prompt(string message){
+    // Promt for user input
+    Console.Write(message);
+    string input = Console.ReadLine();
+    return input;
+}
+
+Console.Clear();
+string userInput = Prompt("Введите элементы массива через пробел: ");
+int[] inputArray = StringToIntArray(userInput);
+int answer = GetNumberPositive(inputArray);
+Console.WriteLine($"Количество чисел больше нуля {answer}");
---
+++ 2_c-sharp/Seminar_6/home_work/task43.cs
@@ -0,0 +1,32 @@
+﻿float[] GetLinesIntersection(float[] arrCoeaf){
+    // Method gets answer
+    if (arrCoeaf[0] == arrCoeaf[2]){
+        Console.WriteLine("k1 не должно равняться k2");
+        return null;
+    }
+    float x = (arrCoeaf[3] - arrCoeaf[1])/(arrCoeaf[0]-arrCoeaf[2]);
+    float y = arrCoeaf[0]*x + arrCoeaf[1];
+    float[] answerArray = {x, y};
+
+    return answerArray;
+}
+
+float[] StringToIntArray(string stringElements)
+{ string[] stringArray = stringElements.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    float[] res = new float[stringArray.Length];
+    for(int i = 0; i < res.Length; i++)
+    { res[i] = float.Parse(stringArray[i]);
+    } return res; }
+
+string Prompt(string message){
+    // Promt for user input
+    Console.Write(message);
+    string input = Console.ReadLine();
+    return input;
+}
+
+Console.Clear();
+string userInput = Prompt("Введите коэффициенты k1, b1, k2, b2 через пробел: ");
+float[] inputArray = StringToIntArray(userInput);
+float[] answer = CalculateIntersection(inputArray);
+Console.WriteLine($"Пересечение линий на x = {answer[0]} y = {answer[1]}");
